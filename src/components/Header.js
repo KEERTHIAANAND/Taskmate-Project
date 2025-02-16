573e0c81ff1e051885e2d24b1b4a25d33b0e4483
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-02-16 23:12:25 +0530

    header initiated

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,14 @@ export const Header = () => {
             <img src={Logo} alt="Taskmate logo" />
             <span>TASKMATE</span>
         </div>
+        <div className="themeSelector">
+            <span className="light"></span>
+            <span className="medium"></span>
+            <span className="dark"></span>
+            <span className="gOne activeTheme"></span>
+            <span className="gTwo"></span>
+            <span className="gThree"></span>
+        </div>
     </header>
   )
 }
