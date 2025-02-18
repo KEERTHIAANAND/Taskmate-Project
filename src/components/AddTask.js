ad8fc3fd8bd9cdf5a4f03d6f79126ecdff0a3e48
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-02-18 22:48:15 +0530

    updated

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,23 +1,26 @@
 export const AddTask = ({tasklist,setTasklist,task,setTask}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
+    //Edit function
     if(task.id){
       const date= new Date();
       const updateTask=tasklist.map((todo)=>(
         todo.id === task.id ? {id: task.id, name:task.name,time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo));
         setTasklist(updateTask);
-        e.target.task.value="";
-    }else{
+        setTask({});
+    }
+    //Delete function
+    else{
       const date=new Date();
     const newTask ={id: date.getTime(), name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`}
     setTasklist([...tasklist, newTask]);
-    e.target.task.value="";
+    setTask({});
     }
   }
   return (
     <section className="addTask">
         <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name} autoComplete="off" placeholder="Task Name" onChange={e=>setTask({...task, name:e.target.value})}/>
+            <input type="text" name="task" value={task.name || ""} autoComplete="off" placeholder="Task Name" onChange={e=>setTask({...task, name:e.target.value})}/>
             <button type="submit">ADD</button>
         </form>
     </section>
